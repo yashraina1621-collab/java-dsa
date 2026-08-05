b7fef8393c3b5293863a27fba1fc5d74c9ec02a4
yashraina1621-collab <yashraina1621@gmail.com>
2026-08-05 22:43:59 +0530

    Add LC 2469 Convert Temperature solution

---
+++ src/math/LC_2469_ConvertTheTemperature.java
@@ -0,0 +1,8 @@
+package math;
+
+public class LC_2469_ConvertTheTemperature {
+
+        public double[] convertTemperature(double celsius) {
+            return new double[] {celsius + 273.15, celsius * 1.8 + 32.00};
+        }
+    }
