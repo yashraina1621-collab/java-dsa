823a6ff0c89f64e4c75348de12abe24d3d409bf8
yashraina1621-collab <yashraina1621@gmail.com>
2026-08-06 09:41:27 +0530

    convertTheTemperature

--- src/math/LC_2469_ConvertTheTemperature.java
+++ src/math/LC_2469_ConvertTheTemperature.java
@@ -1,6 +1,11 @@
 package math;
 
 public class LC_2469_ConvertTheTemperature {
+    /* Platform: LeetCode
+ * Problem: 2469. Convert the Temperature
+ * Difficulty: Easy
+ * Topic: Math
+     */
 
         public double[] convertTemperature(double celsius) {
             return new double[] {celsius + 273.15, celsius * 1.8 + 32.00};
