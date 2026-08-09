7c351999a377c1100c071975e1e7ad448cbde1f4
yashraina1621-collab <yashraina1621@gmail.com>
2026-08-09 22:19:10 +0530

    Added remove duplicates solution

---
+++ src/sorting/LC_26_RemoveDuplicatesfromSortedArray.java
@@ -0,0 +1,29 @@
+package sorting;
+
+public class LC_26_RemoveDuplicatesfromSortedArray {
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 1, 2, 2, 3};
+
+        int i = 0;
+
+        for (int j = 1; j < nums.length; j++) {
+
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+
+        int k = i + 1;
+
+        System.out.println("Unique elements = " + k);
+
+        System.out.print("Array after removing duplicates: ");
+
+        for (int x = 0; x < k; x++) {
+            System.out.print(nums[x] + " ");
+        }
+    }
+}
